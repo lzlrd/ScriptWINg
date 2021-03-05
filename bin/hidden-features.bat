c07335cc5e515b329382949d0e1da899e9a588de
Diab Neiroukh <lazerl0rd@thezest.dev>
2021-03-05 17:23:41 +0000

    hidden-features: Add support for Pen and Ink settings rejuvenation.
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>

--- bin/hidden-features.bat
+++ bin/hidden-features.bat
@@ -80,6 +80,7 @@ rem Settings Enhancements and Rejuvenation
 1>NUL "%~dp0ViVeTool.exe" addconfig 29241208 2
 1>NUL "%~dp0ViVeTool.exe" addconfig 29241309 2
 1>NUL "%~dp0ViVeTool.exe" addconfig 25810627 2
+1>NUL "%~dp0ViVeTool.exe" addconfig 29673992 2
 rem Acrylic Task View in Timeline
 1>NUL "%~dp0ViVeTool.exe" addconfig 12520383 2
 rem Device Health Improvements
